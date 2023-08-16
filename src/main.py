8253fbeba8f3afa05234963a3fa11d694cceb066
KevinBeltran23 <kevinbeltran2437@gmail.com>
2023-08-16 13:22:58 -0700

    test

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import 
+import sys
+
+from const import *
+
+class Main:
+
+    def __init__(self):
+        print("hello")
+
+    def mainloop(self):
+        print("world")
+
+
+main = Main()
+main.mainloop()
